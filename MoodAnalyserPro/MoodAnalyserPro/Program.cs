e125eae0521f4d047058eaee00cdc243eb87d56c
VikasBenki <vikasmsbenki@gmail.com>
2022-03-10 22:56:25 +0530

    [VikasBenki] Adding the welcome message

---
+++ MoodAnalyserPro/MoodAnalyserPro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Mood annalyser Problem");
+            Console.ReadLine();
+        }
+    }
+}
